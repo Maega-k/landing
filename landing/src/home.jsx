ab910886192429ce03e49bf21871ca358e3342e4
Maega Khama <153864938+Maega-k@users.noreply.github.com>
2025-09-23 10:21:12 +0100

    Add files via upload

---
+++ landing/src/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './navbar'
+import Hero from './hero' 
+import Reviews from './reviews'
+import Footer from './footer'
+
+
+const Home = () => {
+  return (
+    <>
+    
+    <Hero />
+    <Reviews />
+    <Footer />
+    </>
+  )
+}
+
+export default Home

2f16625b74c260a9bb9b5b8bcf959c87c30a06ec
Maega Khama <153864938+Maega-k@users.noreply.github.com>
2025-09-24 09:58:44 +0100

    Update products.jsx

--- landing/src/products.jsx
+++ landing/src/products.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from 'framer-motion';
-import Navbar from './navbar';
+import Navbar from "./navbar";
 
 function Products(){
     return(
@@ -9,7 +9,7 @@ function Products(){
         <section>
             <div>
                 <motion.h1
-                    className="text-center text-4xl font-extrabold text-lightorange p-7"
+                    className="text-center text-4xl font-extrabold text-bluee p-7"
                     initial={{ opacity: 0, y: -40 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.7 }}
